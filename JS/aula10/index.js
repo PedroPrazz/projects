9cbc4c8ca1934b511b8ad221c84f2e4922c16ccc
PedroPrazz <pedrogprazeres@gmail.com>
2025-03-28 16:49:00 -0300

    Operadores aritméticos, de atribuição e incremento

---
+++ JS/aula10/index.js
@@ -0,0 +1,29 @@
+let contador = 1;
+contador++;
+
+let contador2 = 0;
+contador2 += 2;
+
+let contadorNegativo = 10;
+contadorNegativo--;
+
+const num1 = 2;
+const num2 = 10;
+console.log(num1 ** num2);
+
+const num3 = 53;
+const num4 = 5;
+console.log(num3 * num4);
+
+const num5 = 22;
+const num6 = 18;
+console.log(num5 + num6);
+
+const num7 = 25;
+const num8 = 10;
+console.log(num7 % num8);
+
+console.log((num1 +  num2) * num5);
+console.log(contador);
+console.log(contadorNegativo);
+console.log(contador2);
