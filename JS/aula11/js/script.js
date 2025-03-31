6d019deee47dd63c6f019c059331553fdab756dc
PedroPrazz <pedrogprazeres@gmail.com>
2025-03-31 14:51:25 -0300

    exercício alert, prompt

---
+++ JS/aula11/js/script.js
@@ -0,0 +1,10 @@
+
+let num1 = prompt('Digite primeiro número:');
+let num2 = prompt('Digite segundo número:');
+
+num1 = parseFloat(num1);
+num2 = parseFloat(num2);
+
+let resultado = num1 + num2;
+alert(`O resultado é: ${resultado}`);
+
